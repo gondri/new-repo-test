e8ec23f879a1437518c6de8ed81b9388edce224c
vondracek <vondracek@cngroup.dk>
2018-01-14 10:44:37 +0100

    adding spring boot

--- src/test/groovy/AppTest.groovy
+++ src/test/groovy/AppTest.groovy
@@ -6,13 +6,5 @@ import spock.lang.Specification
 class AppTest extends Specification {
 
     def "application has a greeting"() {
-        setup:
-        def app = new App()
-
-        when:
-        def result = app.greeting
-
-        then:
-        result != null
     }
 }
